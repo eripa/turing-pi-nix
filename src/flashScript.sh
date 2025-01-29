510a07a855a7ad27b91d5509e7ed3d32ccc52a50
Wout Swinkels <github@woutswinkels.com>
2025-01-29 20:21:05 +0100

    Copy the image to the SD card inserted in the Turing Pi and flash the node from the SD card

--- src/flashScript.sh
+++ src/flashScript.sh
@@ -12,10 +12,6 @@ IP=$2
 USER=$3
 PASSWORD=$4
 
-echo "Reboot the BMC chip..."
-tpi reboot --host "$IP" --user "$USER" --password "$PASSWORD"
-sleep 10
-
 echo "Building the Raspberry Pi image..."
 nix build .#images.rpi-example
 
@@ -27,8 +23,28 @@ if [[ ! -f "$IMG_PATH" ]]; then
 fi
 
 UNCOMPRESSED_IMG=rpiImage.img
-unzstd "$IMG_PATH" -o "$UNCOMPRESSED_IMG"
+unzstd -f "$IMG_PATH" -o "$UNCOMPRESSED_IMG"
+
+echo "Checking if the SD card is already mounted on the Turing Pi..."
+sshpass -p "$PASSWORD" ssh -T "$USER@$IP" << 'EOF'
+if ! mountpoint -q /mnt/sdcard; then
+  echo "Mounting the SD card..."
+  if [ -b /dev/mmcblk0p1 ]; then
+    mount /dev/mmcblk0p1 /mnt/sdcard
+  elif [ -b /dev/mmcblk0 ]; then
+    mount /dev/mmcblk0 /mnt/sdcard
+  else
+    echo "Error: SD card device not found."
+    exit 1
+  fi
+else
+  echo "SD card is already mounted."
+fi
+EOF
+
+echo "Copy the image to the SD card..."
+sshpass -p "$PASSWORD" scp "./$UNCOMPRESSED_IMG" "$USER@$IP":/mnt/sdcard/
 
 echo "Flashing the image to the Turing Pi node..."
-tpi flash -n "$NODE" --host "$IP" --user "$USER" --password "$PASSWORD" -i "./$UNCOMPRESSED_IMG"
+tpi flash -n "$NODE" --host "$IP" --user "$USER" --password "$PASSWORD" -l -i "/mnt/sdcard/$UNCOMPRESSED_IMG"
 tpi power -n "$NODE" --host "$IP" --user "$USER" --password "$PASSWORD" on
